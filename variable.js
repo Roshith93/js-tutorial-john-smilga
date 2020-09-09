10747a14d9decad283d451ec6b18b8027f7db4d0
Roshith R <r.roshith@gmail.com>
2020-09-09 13:50:05 +0530

    variables naming rules

--- variable.js
+++ variable.js
@@ -5,3 +5,11 @@ console.log(name)
 // assigning variable later;
 name = "Hello"
 console.log(name)
+
+// varible naming rules
+// can contain digit, letters, $ , _
+// must start with letter, $ or _
+// cant use reserved keywords like this, function
+// variables are case sensitive
+// length of the variable can be as much as you can
+// it can follow camelCaseWay or underscore_way
