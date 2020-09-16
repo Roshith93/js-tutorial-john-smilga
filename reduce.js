33bed034bb08d7dd013e3c7b72164e2f97f3b36f
Roshith R <r.roshith@gmail.com>
2020-09-16 18:02:33 +0530

    Creating new array from array of objects

--- reduce.js
+++ reduce.js
@@ -21,3 +21,32 @@ const totalSalary = person.reduce((acc, currentVal) => {
   return acc
 }, 0)
 console.log(totalSalary)
+
+// Bonding arrays contained in an array of objects using the spread operator and initialValue
+// friends - an array of objects 
+// where object field "books" is a list of favorite books 
+let friends = [{
+  name: 'Anna',
+  books: ['Bible', 'Harry Potter'],
+  age: 21
+}, {
+  name: 'Bob',
+  books: ['War and peace', 'Romeo and Juliet'],
+  age: 26
+}, {
+  name: 'Alice',
+  books: ['The Lord of the Rings', 'The Shining'],
+  age: 18
+}]
+
+// allbooks - list which will contain all friends' books +  
+// additional list contained in initialValue
+let allbooks = friends.reduce(function (accumulator, currentValue) {
+  return [...accumulator, ...currentValue.books]
+}, ['Alphabet'])
+console.log(allbooks)
+// allbooks = [
+//   'Alphabet', 'Bible', 'Harry Potter', 'War and peace', 
+//   'Romeo and Juliet', 'The Lord of the Rings',
+//   'The Shining'
+// ]
