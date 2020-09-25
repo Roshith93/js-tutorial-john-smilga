e90783f268a16ae5b1ca53d2009321af111ed68c
Roshith R <r.roshith@gmail.com>
2020-09-25 13:25:41 +0530

    query selector

---
+++ Dom/querySelector.js
@@ -0,0 +1,4 @@
+const querySel = document.querySelector('.unorderedList')
+// More Specified Way
+const querySelDiv = document.querySelector('div.main ul.unorderedList')
+querySelDiv.style.backgroundColor = "red"
