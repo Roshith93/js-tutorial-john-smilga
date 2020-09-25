ffb99ba1bee296df4ce96d33418ccb298b45e31a
Roshith R <r.roshith@gmail.com>
2020-09-25 11:57:31 +0530

    Get element by class Name

---
+++ Dom/getEleByClass.js
@@ -0,0 +1,5 @@
+const unorderedList = document.getElementsByClassName('unorderedList')
+unorderedList[unorderedList.length - 1].style.backgroundColor ="#3178c6"
+console.log('====================================');
+console.log(unorderedList);
+console.log('====================================');
