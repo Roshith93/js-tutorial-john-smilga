9560d7f2b7ef5baf357f827ce1f3f8a57a80bb81
Roshith R <r.roshith@gmail.com>
2020-09-25 11:48:47 +0530

    Get element by tab name

---
+++ Dom/getEleTab.js
@@ -0,0 +1,7 @@
+const byTag = document.getElementsByTagName('button')
+byTag[0].style.backgroundColor = "blue";
+// byTag[0].style.cssText = "white";
+console.log(byTag)
+const lists  = document.getElementsByTagName('li')
+const arrayList = [...lists]
+console.log(arrayList)
