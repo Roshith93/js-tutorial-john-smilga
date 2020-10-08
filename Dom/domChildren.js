dce38fa95228d70a3d371d47b45e2098304026aa
Roshith R <r.roshith@gmail.com>
2020-10-08 12:06:51 +0530

    Dom childrens

---
+++ Dom/domChildren.js
@@ -0,0 +1,5 @@
+const getChildren = document.querySelector('.orderedList');
+const withChildNodes = getChildren.childNodes;
+const withChildren = getChildren.children;
+console.log(withChildNodes)
+console.log(withChildren)
