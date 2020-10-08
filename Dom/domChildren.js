7c860a7f1ab153389c67a443961aa165a0fb0f58
Roshith R <r.roshith@gmail.com>
2020-10-08 12:25:29 +0530

    firstchild ,firstelementchild

--- Dom/domChildren.js
+++ Dom/domChildren.js
@@ -3,3 +3,8 @@ const withChildNodes = getChildren.childNodes;
 const withChildren = getChildren.children;
 console.log(withChildNodes)
 console.log(withChildren)
+
+// first child
+// last child
+console.log(getChildren.firstChild) //text node
+console.log(getChildren.firstElementChild) //<li>a</li>
