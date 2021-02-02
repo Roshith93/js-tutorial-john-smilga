377991c67243bf4cf534dce1de695c5540557da6
Roshith R <r.roshith@gmail.com>
2021-02-02 12:47:16 +0530

    event handler and event listener

--- Events/click.js
+++ Events/click.js
@@ -1,6 +1,7 @@
 const getDiv = document.querySelector('#main')
 const position  = getDiv.querySelector('.root')
-console.log("position")
+const position2  = getDiv.querySelector('.root2')
+console.log(position2)
 
 const hoverEffect = () => {
  const newDoc = document.createElement('h1')
@@ -9,3 +10,12 @@ const hoverEffect = () => {
  position.appendChild(newDoc)
 }
 const newTest = () => alert("hello")
+
+// event handler
+position.onclick =  () => alert("first")
+position.onclick =  () => alert("second")
+position.onmouseover = () =>alert("third")
+position.onmouseover = () =>alert("4th")
+
+// event listener
+position2.addEventListener("mouseover", (e) => alert("hello"))
