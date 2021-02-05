1b7212e32530a4360b6464c87b1d9a16e394f87e
Roshith R <r.roshith@gmail.com>
2021-02-05 06:53:24 +0530

    innnerHTML

--- Dom/innerText&textContent.js
+++ Dom/innerText&textContent.js
@@ -4,4 +4,16 @@ const getInnerHTML = textCtnt.innerHTML; //show all descendants as HTML elements
 const getInnerText = textCtnt.innerText; //will skip css and script html elements
 const getTextContext = textCtnt.textContent; // show text contents
 
-console.log(getTextContext);
+console.log(getInnerHTML);
+var htmlContent;
+var finalhtmlContent;
+textCtnt.innerHTML = finalhtmlContent
+htmlContent = `<ul class="unorderedList1">
+<li class="alphabet">a</li>
+<li class="alphabet">b</li>
+<li class="alphabet">c</li>
+<li class="alphabet">d</li>
+<li class="alphabet">en</li>
+</ul>`
+
+finalhtmlContent += htmlContent
